f6648010c55a24445d9266e595ac4bc18162b83b
Nguyen Hoang Anh <hoanganh8d@gmail.com>
2023-05-02 23:20:50 +0700

    Init project

---
+++ src/main/java/com/kythuatphanmem/quanlydancu/QuanlydancuApplication.java
@@ -0,0 +1,13 @@
+package com.kythuatphanmem.quanlydancu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlydancuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlydancuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kythuatphanmem/quanlydancu/QuanlydancuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kythuatphanmem.quanlydancu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlydancuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
